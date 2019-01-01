1dabc705f994b5587b084beaef561fd6fa9c0b3a
Michal Bubnár <michalbb1@gmail.com>
2019-01-01 18:38:23 +0100

    Renamed StaticMesh3D files

---
+++ common_classes/static_meshes/staticMesh3D.cpp
@@ -0,0 +1,87 @@
+#include "staticMesh3D.h"
+
+#include <glm/glm.hpp>
+
+namespace static_meshes {
+
+const int StaticMesh3D::POSITION_ATTRIBUTE_INDEX           = 0;
+const int StaticMesh3D::TEXTURE_COORDINATE_ATTRIBUTE_INDEX = 1;
+const int StaticMesh3D::NORMAL_ATTRIBUTE_INDEX             = 2;
+
+StaticMesh3D::StaticMesh3D(bool withPositions, bool withTextureCoordinates, bool withNormals)
+	: _hasPositions(withPositions)
+	, _hasTextureCoordinates(withTextureCoordinates)
+	, _hasNormals(withNormals) {}
+
+void StaticMesh3D::deleteMesh()
+{
+	if (!_isInitialized) {
+		return;
+	}
+
+	glDeleteVertexArrays(1, &_vao);
+	_vbo.deleteVBO();
+
+	_isInitialized = false;
+}
+
+bool StaticMesh3D::hasPositions() const
+{
+	return _hasPositions;
+}
+
+bool StaticMesh3D::hasTextureCoordinates() const
+{
+	return _hasTextureCoordinates;
+}
+
+bool StaticMesh3D::hasNormals() const
+{
+	return _hasNormals;
+}
+
+int StaticMesh3D::getVertexByteSize() const
+{
+	int result = 0;
+	if (hasPositions()) {
+		result += sizeof(glm::vec3);
+	}
+	if (hasTextureCoordinates()) {
+		result += sizeof(glm::vec2);
+	}
+	if (hasNormals()) {
+		result += sizeof(glm::vec3);
+	}
+
+	return result;
+}
+
+void StaticMesh3D::setVertexAttributesPointers(int numVertices)
+{
+	uint32_t offset = 0;
+	if (hasPositions())
+	{
+		glEnableVertexAttribArray(POSITION_ATTRIBUTE_INDEX);
+		glVertexAttribPointer(POSITION_ATTRIBUTE_INDEX, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)offset);
+
+		offset += sizeof(glm::vec3)*numVertices;
+	}
+
+	if (hasTextureCoordinates())
+	{
+		glEnableVertexAttribArray(TEXTURE_COORDINATE_ATTRIBUTE_INDEX);
+		glVertexAttribPointer(TEXTURE_COORDINATE_ATTRIBUTE_INDEX, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), (void*)offset);
+
+		offset += sizeof(glm::vec2)*numVertices;
+	}
+
+	if (hasNormals())
+	{
+		glEnableVertexAttribArray(NORMAL_ATTRIBUTE_INDEX);
+		glVertexAttribPointer(NORMAL_ATTRIBUTE_INDEX, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)offset);
+
+		offset += sizeof(glm::vec3)*numVertices;
+	}
+}
+
+} // namespace static_meshes
--- common_classes/static_meshes/staticMesh3D.h
+++ common_classes/static_meshes/staticMesh3D.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../vertexBufferObject.h"
+
 namespace static_meshes {
 
 class StaticMesh3D
@@ -11,8 +13,8 @@ public:
 	static const int TEXTURE_COORDINATE_ATTRIBUTE_INDEX;
 	static const int NORMAL_ATTRIBUTE_INDEX;
 
-	virtual void render() = 0;
-	virtual void release() = 0;
+	virtual void render() const = 0;
+	void deleteMesh();
 
 	bool hasPositions() const;
 	bool hasTextureCoordinates() const;
@@ -20,11 +22,16 @@ public:
 	int getVertexByteSize() const;
 
 protected:
-	bool _hasPositions;
-	bool _hasTextureCoordinates;
-	bool _hasNormals;
+	bool _hasPositions = false;
+	bool _hasTextureCoordinates = false;
+	bool _hasNormals = false;
+
+	bool _isInitialized = false;
+	GLuint _vao = 0;
+	VertexBufferObject _vbo;
 
 	virtual void initializeData() = 0;
+	void setVertexAttributesPointers(int numVertices);
 };
 
 }; // namespace static_meshes
