fb92f0efe3424b30d6ba03d86cd0ee87bbe2f50f
Michal Bubnár <michalbb1@gmail.com>
2019-01-02 12:33:16 +0100

    Added cube class documentation

--- common_classes/static_meshes/primitives/cube.h
+++ common_classes/static_meshes/primitives/cube.h
@@ -8,13 +8,16 @@
 
 namespace static_meshes {
 
-const int CUBE_FRONT_FACE = 1;
-const int CUBE_BACK_FACE = 1 << 1;
-const int CUBE_LEFT_FACE = 1 << 2;
-const int CUBE_RIGHT_FACE = 1 << 3;
-const int CUBE_TOP_FACE = 1 << 4;
-const int CUBE_BOTTOM_FACE = 1 << 5;
+const int CUBE_FRONT_FACE = 1; //!< Bitmask to render cube front face
+const int CUBE_BACK_FACE = 1 << 1; //!< Bitmask to render cube back face
+const int CUBE_LEFT_FACE = 1 << 2; //!< Bitmask to render cube left face
+const int CUBE_RIGHT_FACE = 1 << 3; //!< Bitmask to render cube right face
+const int CUBE_TOP_FACE = 1 << 4; //!< Bitmask to render cube top face
+const int CUBE_BOTTOM_FACE = 1 << 5; //!< Bitmask to render cube bottom face
 
+/**
+	Cube static mesh of unit size.
+*/
 class Cube : public StaticMesh3D
 {
 public:
@@ -23,9 +26,9 @@ public:
 	void render() const override;
 	void renderFaces(int facesBitmask) const;
 
-	static glm::vec3 vertices[36];
-	static glm::vec2 textureCoordinates[6];
-	static glm::vec3 normals[6];
+	static glm::vec3 vertices[36]; //!< Array of mesh vertices
+	static glm::vec2 textureCoordinates[6]; //!< Array of mesh texture coordinates
+	static glm::vec3 normals[6]; //!< Array of mesh normals
 
 private:
 	void initializeData() override;
