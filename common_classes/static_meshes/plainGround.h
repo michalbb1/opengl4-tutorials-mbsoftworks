011e7a0c3caf321fc0cacc4802123d067a81dfd7
Michal Bubnár <michalbb1@gmail.com>
2019-01-02 12:34:10 +0100

    Added plain ground documentation

--- common_classes/static_meshes/plainGround.h
+++ common_classes/static_meshes/plainGround.h
@@ -12,18 +12,21 @@
 
 namespace static_meshes {
 
+/**
+	Plain ground static mesh.
+*/
 class PlainGround : public StaticMesh3D
 {
 public:
 	PlainGround(bool withPositions = true, bool withTextureCoordinates = true, bool withNormals = true);
 
-	static glm::vec3 vertices[4];
-	static glm::vec2 textureCoordinates[4];
+	static glm::vec3 vertices[4]; //!< Array of mesh vertices
+	static glm::vec2 textureCoordinates[4]; //!< Array of mesh texture coordinates
 
 	void render() const override;
 
 protected:
-	void initializeData() override;
+	virtual void initializeData() override;
 };
 
 } // namespace static_meshes
