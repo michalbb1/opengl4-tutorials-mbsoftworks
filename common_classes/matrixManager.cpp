c12b5884150ebf7af5a689586fa62017924012a1
Michal Bubnár <michalbb1@gmail.com>
2019-01-02 14:42:56 +0100

    Added orthographic projection matrix to MatrixManager

--- common_classes/matrixManager.cpp
+++ common_classes/matrixManager.cpp
@@ -8,20 +8,30 @@ MatrixManager& MatrixManager::getInstance()
 
 const glm::mat4& MatrixManager::getProjectionMatrix() const
 {
-	return projectionMatrix;
+	return _projectionMatrix;
+}
+
+const glm::mat4& MatrixManager::getOrthoMatrix() const
+{
+	return _orthoProjectionMatrix;
 }
 
 const glm::mat4& MatrixManager::getViewMatrix() const
 {
-	return viewMatrix;
+	return _viewMatrix;
 }
 
 void MatrixManager::setProjectionMatrix(const glm::mat4& projectionMatrix)
 {
-	this->projectionMatrix = projectionMatrix;
+	_projectionMatrix = projectionMatrix;
+}
+
+void MatrixManager::setOrthoProjectionMatrix(const glm::mat4& orthoProjectionMatrix)
+{
+	_orthoProjectionMatrix = orthoProjectionMatrix;
 }
 
 void MatrixManager::setViewMatrix(const glm::mat4& viewMatrix)
 {
-	this->viewMatrix = viewMatrix;
+	_viewMatrix = viewMatrix;
 }
--- common_classes/matrixManager.h
+++ common_classes/matrixManager.h
@@ -11,14 +11,17 @@ public:
 	static MatrixManager& getInstance();
 
 	const glm::mat4& getProjectionMatrix() const;
+	const glm::mat4& getOrthoMatrix() const;
 	const glm::mat4& getViewMatrix() const;
 
 	void setProjectionMatrix(const glm::mat4& projectionMatrix);
+	void setOrthoProjectionMatrix(const glm::mat4& orthoProjectionMatrix);
 	void setViewMatrix(const glm::mat4& viewMatrix);
 
 private:
 	MatrixManager() {}
 
-	glm::mat4 projectionMatrix;
-	glm::mat4 viewMatrix;
+	glm::mat4 _projectionMatrix;
+	glm::mat4 _orthoProjectionMatrix;
+	glm::mat4 _viewMatrix;
 };
