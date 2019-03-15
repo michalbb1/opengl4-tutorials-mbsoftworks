78d61b230927d5b9f15ebc054e57f93c5762e884
Michal Bubnar <michalbb1@gmail.com>
2019-03-15 09:09:56 +0100

    Added shader structs documentation

--- common_classes/shader_structs/ambientLight.h
+++ common_classes/shader_structs/ambientLight.h
@@ -8,11 +8,16 @@ struct AmbientLight : ShaderStruct
 {
 	AmbientLight(const glm::vec3& color, const bool isOn = true);
 
+	//* \brief Sets ambient light in a shader program. */
 	void setUniform(ShaderProgram& shaderProgram, const std::string& uniformName) const override;
+
+	/** \brief  Gets the final contributed color of this ambient light (depending if light is on or off).
+	*   \return The ambient light color contribution.
+	*/
 	glm::vec3 getColorContribution() const;
 
-	glm::vec3 color;
-	bool isOn;
+	glm::vec3 color; //!< Color of the ambient light
+	bool isOn; //!< Flag telling, if the light is on
 };
 
 } // namespace shader_structs
--- common_classes/shader_structs/diffuseLight.h
+++ common_classes/shader_structs/diffuseLight.h
@@ -8,12 +8,13 @@ struct DiffuseLight : ShaderStruct
 {
 	DiffuseLight(const glm::vec3& color, const glm::vec3& direction, const float factor, const bool isOn = true);
 
+	//* \brief Sets diffuse light in a shader program. */
 	void setUniform(ShaderProgram& shaderProgram, const std::string& uniformName) const override;
 
-	glm::vec3 color;
-	glm::vec3 direction;
-	float factor;
-	bool isOn;
+	glm::vec3 color; //!< Color of the diffuse light
+	glm::vec3 direction; //!< Direction of the diffuse light
+	float factor; //!< Factor to multiply dot product with (strength of light)
+	bool isOn; //!< Flag telling, if the light is on
 };
 
 } // namespace shader_structs
