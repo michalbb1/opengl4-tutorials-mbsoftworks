d1796c2237bee223dec825a8dab6f728c763c2dc
Michal Bubnár <michalbb1@gmail.com>
2020-05-25 10:05:52 +0200

    Cylinder implementation

---
+++ common_classes/static_meshes_3D/primitives/cylinder.h
@@ -0,0 +1,47 @@
+#pragma once
+
+// Common classes
+#include "../staticMesh3D.h"
+
+namespace static_meshes_3D {
+
+/**
+* Cylinder static mesh with given radius, number of slices and height.
+*/
+class Cylinder : public StaticMesh3D
+{
+public:
+    Cylinder(float radius, int numSlices, float height,
+        bool withPositions = true, bool withTextureCoordinates = true, bool withNormals = true);
+
+    void render() const override;
+    void renderPoints() const override;
+
+    /**
+     * Gets cylinder radius.
+     */
+    float getRadius() const;
+
+    /**
+     * Gets number of cylinder slices.
+     */
+    int getSlices() const;
+
+    /**
+     * Gets cylinder height.
+     */
+    float getHeight() const;
+
+private:
+    float _radius; // Cylinder radius (distance from the center of cylinder to surface)
+    int _numSlices; // Number of cylinder slices
+    float _height; // Height of the cylinder
+
+    int _numVerticesSide; // How many vertices to render side of the cylinder
+    int _numVerticesTopBottom; // How many vertices to render top / bottom of the cylinder
+    int _numVerticesTotal; // Just a sum of both numbers above
+    
+    void initializeData() override;
+};
+
+} // namespace static_meshes_3D
