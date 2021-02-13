3804b12edccfeaa2b13ce170695f0893e00c3c78
Michal Bubnar <michalbb1@gmail.com>
2021-02-13 08:27:24 +0100

    Updated windows_setup.sh script for VS2019

--- windows_setup.sh
+++ windows_setup.sh
@@ -4,39 +4,44 @@
 echo "Updating all submodules..."
 git submodule update --init --recursive
 
-# FreeType2 pre-built libraries downloads
-echo "Creating subdirectories for FreeType2 libraries..."
-mkdir -p "dependencies/freetype2-prebuilt/vs2017/libs32"
-mkdir -p "dependencies/freetype2-prebuilt/vs2017/libs64"
+# Store the root for pre-built files in a variable
+prebuilt_root="https://www.mbsoftworks.sk/meta/prebuilt"
 
-echo "Downloading pre-built FreeType2 library for VS2017 (32-bit version)..."
-curl -o "dependencies/freetype2-prebuilt/vs2017/libs32/freetype2.lib" "http://www.mbsoftworks.sk/meta/prebuilt/freetype2/vs2017/libs32/freetype2.lib"
-echo "Downloading pre-built FreeType2 library for VS2017 (64-bit version)..."
-curl -o "dependencies/freetype2-prebuilt/vs2017/libs64/freetype2_64.lib" "http://www.mbsoftworks.sk/meta/prebuilt/freetype2/vs2017/libs64/freetype2_64.lib"
+# FreeType2 pre-built libraries downloads
+prebuilt_root_freetype="${prebuilt_root}/freetype2/vs2019"
+echo "Creating subdirectories for FreeType2 libraries..."
+mkdir -p "dependencies/freetype2-prebuilt/vs2019/libs32"
+mkdir -p "dependencies/freetype2-prebuilt/vs2019/libs64"
+
+echo "Downloading pre-built FreeType2 library for VS2019 (32-bit version)..."
+curl -o "dependencies/freetype2-prebuilt/vs2019/libs32/freetype.lib" "${prebuilt_root_freetype}/libs32/freetype.lib"
+echo "Downloading pre-built FreeType2 library for VS2019 (64-bit version)..."
+curl -o "dependencies/freetype2-prebuilt/vs2019/libs64/freetype.lib" "${prebuilt_root_freetype}/libs64/freetype.lib"
 
 echo "Creating subdirectories for Assimp libraries..."
-mkdir -p "dependencies/assimp-prebuilt/vs2017/libs32/debug"
-mkdir -p "dependencies/assimp-prebuilt/vs2017/libs32/release"
-mkdir -p "dependencies/assimp-prebuilt/vs2017/libs64/debug"
-mkdir -p "dependencies/assimp-prebuilt/vs2017/libs64/release"
+mkdir -p "dependencies/assimp-prebuilt/vs2019/libs32/debug"
+mkdir -p "dependencies/assimp-prebuilt/vs2019/libs32/release"
+mkdir -p "dependencies/assimp-prebuilt/vs2019/libs64/debug"
+mkdir -p "dependencies/assimp-prebuilt/vs2019/libs64/release"
 
 # Assimp pre-built libraries downloads
-echo "Downloading Assimp pre-built libraries for VS2017 / 32-bit debug version..."
-curl -o "dependencies/assimp-prebuilt/vs2017/libs32/debug/assimp-vc141-mtd.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs32/debug/assimp-vc141-mtd.lib"
-curl -o "dependencies/assimp-prebuilt/vs2017/libs32/debug/IrrXMLd.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs32/debug/IrrXMLd.lib"
-curl -o "dependencies/assimp-prebuilt/vs2017/libs32/debug/zlibstaticd.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs32/debug/zlibstaticd.lib"
+prebuilt_root_assimp="${prebuilt_root}/assimp/vs2019"
+echo "Downloading Assimp pre-built libraries for VS2019 / 32-bit debug version..."
+curl -o "dependencies/assimp-prebuilt/vs2019/libs32/debug/assimp-vc142-mtd.lib" "${prebuilt_root_assimp}/libs32/debug/assimp-vc142-mtd.lib"
+curl -o "dependencies/assimp-prebuilt/vs2019/libs32/debug/IrrXMLd.lib" "${prebuilt_root_assimp}/libs32/debug/IrrXMLd.lib"
+curl -o "dependencies/assimp-prebuilt/vs2019/libs32/debug/zlibstaticd.lib" "${prebuilt_root_assimp}/libs32/debug/zlibstaticd.lib"
 
-echo "Downloading Assimp pre-built libraries for VS2017 / 64-bit debug version..."
-curl -o "dependencies/assimp-prebuilt/vs2017/libs64/debug/assimp-vc141-mtd.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs64/debug/assimp-vc141-mtd.lib"
-curl -o "dependencies/assimp-prebuilt/vs2017/libs64/debug/IrrXMLd.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs64/debug/IrrXMLd.lib"
-curl -o "dependencies/assimp-prebuilt/vs2017/libs64/debug/zlibstaticd.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs64/debug/zlibstaticd.lib"
+echo "Downloading Assimp pre-built libraries for VS2019 / 64-bit debug version..."
+curl -o "dependencies/assimp-prebuilt/vs2019/libs64/debug/assimp-vc142-mtd.lib" "${prebuilt_root_assimp}/libs64/debug/assimp-vc142-mtd.lib"
+curl -o "dependencies/assimp-prebuilt/vs2019/libs64/debug/IrrXMLd.lib" "${prebuilt_root_assimp}/libs64/debug/IrrXMLd.lib"
+curl -o "dependencies/assimp-prebuilt/vs2019/libs64/debug/zlibstaticd.lib" "${prebuilt_root_assimp}/libs64/debug/zlibstaticd.lib"
 
-echo "Downloading Assimp pre-built libraries for VS2017 / 32-bit release version..."
-curl -o "dependencies/assimp-prebuilt/vs2017/libs32/release/assimp-vc141-mt.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs32/release/assimp-vc141-mt.lib"
-curl -o "dependencies/assimp-prebuilt/vs2017/libs32/release/IrrXML.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs32/release/IrrXML.lib"
-curl -o "dependencies/assimp-prebuilt/vs2017/libs32/release/zlibstatic.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs32/release/zlibstatic.lib"
+echo "Downloading Assimp pre-built libraries for VS2019 / 32-bit release version..."
+curl -o "dependencies/assimp-prebuilt/vs2019/libs32/release/assimp-vc142-mt.lib" "${prebuilt_root_assimp}/libs32/release/assimp-vc142-mt.lib"
+curl -o "dependencies/assimp-prebuilt/vs2019/libs32/release/IrrXML.lib" "${prebuilt_root_assimp}/libs32/release/IrrXML.lib"
+curl -o "dependencies/assimp-prebuilt/vs2019/libs32/release/zlibstatic.lib" "${prebuilt_root_assimp}/libs32/release/zlibstatic.lib"
 
-echo "Downloading Assimp pre-built libraries for VS2017 / 64-bit release version..."
-curl -o "dependencies/assimp-prebuilt/vs2017/libs64/release/assimp-vc141-mt.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs64/release/assimp-vc141-mt.lib"
-curl -o "dependencies/assimp-prebuilt/vs2017/libs64/release/IrrXML.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs64/release/IrrXML.lib"
-curl -o "dependencies/assimp-prebuilt/vs2017/libs64/release/zlibstatic.lib" "http://www.mbsoftworks.sk/meta/prebuilt/assimp/vs2017/libs64/release/zlibstatic.lib"
+echo "Downloading Assimp pre-built libraries for VS2019 / 64-bit release version..."
+curl -o "dependencies/assimp-prebuilt/vs2019/libs64/release/assimp-vc142-mt.lib" "${prebuilt_root_assimp}/libs64/release/assimp-vc142-mt.lib"
+curl -o "dependencies/assimp-prebuilt/vs2019/libs64/release/IrrXML.lib" "${prebuilt_root_assimp}/libs64/release/IrrXML.lib"
+curl -o "dependencies/assimp-prebuilt/vs2019/libs64/release/zlibstatic.lib" "${prebuilt_root_assimp}/libs64/release/zlibstatic.lib"
